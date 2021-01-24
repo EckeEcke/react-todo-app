bbcca72521a0fa3450b0cb7eb9147cf2b8cc9dee
Christian Eckardt <eckardt.christian@web.de>
2021-01-25 00:11:45 +0100

    added delete btn and confirm method

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './App.css';
 import Navigation from "./Components/Navigation.jsx";
 import Form from "./Components/Form.jsx";
@@ -6,84 +6,107 @@ import Todolist from "./Components/Todolist.jsx";
 
 
 export default function App() {
-  
+
   const [todos, setAllTodos] = useState(JSON.parse(localStorage.getItem("todos")) || []);
   const [todoID, setID] = useState(localStorage.getItem("id") || 0);
- 
-  
+
 
   const addTodo = text => {
-    if(text.length>0){
+    if (text.length > 0) {
 
       setID(() => {
-        const newID = parseInt(todoID)+1;
+        const newID = parseInt(todoID) + 1;
         localStorage.setItem("id", newID);
         return newID;
-    })
+      })
 
-      const newTodos = [...todos, {text: text, isOpen: true, id: todoID}];
-      
-      setAllTodos(() => {
-        localStorage.setItem("todos", JSON.stringify(newTodos));
-        return newTodos;
+      const newTodos = [...todos, { text: text, isOpen: true, id: todoID }];
+
+      const sortedTodos = newTodos.sort(function (a, b) {
+        if (a.text < b.text) {
+          return -1;
+        }
+        if (a.text > b.text) {
+          return 1;
+        }
+        return 0;
       });
-      
-  }}
 
 
 
- function removeTodo(event) {
-      const foundIndex = todos.findIndex(todo => todo.id == event.currentTarget.id);
-      let listToSplice = [...todos];
-      listToSplice.splice(foundIndex, 1);
       setAllTodos(() => {
-        localStorage.setItem("todos", JSON.stringify(listToSplice));
-        return listToSplice;
+        localStorage.setItem("todos", JSON.stringify(sortedTodos));
+        console.log(sortedTodos);
+        return sortedTodos;
       });
+
+    }
   }
 
-  function lineText(event){
-    const foundIndex = todos.findIndex(todo => todo.id == event.currentTarget.id);
 
-    todos[foundIndex].isOpen = !todos[foundIndex].isOpen
-      setAllTodos(() => {
-        localStorage.setItem("todos", JSON.stringify([...todos]));
-        return [...todos];
+
+  const removeTodo = event => {
+    const foundIndex = todos.findIndex(todo => todo.id == event.currentTarget.id);
+    let listToSplice = [...todos];
+    listToSplice.splice(foundIndex, 1);
+
+
+    setAllTodos(() => {
+      localStorage.setItem("todos", JSON.stringify(listToSplice));
+      return listToSplice;
     });
   }
 
+  const lineText = event => {
+    const foundIndex = todos.findIndex(todo => todo.id == event.currentTarget.id);
+
+    todos[foundIndex].isOpen = !todos[foundIndex].isOpen
+    setAllTodos(() => {
+      localStorage.setItem("todos", JSON.stringify([...todos]));
+      return [...todos];
+    });
+  }
+
+  const deleteList = () => {
+    var confirmDelete = window.confirm("Do you want to delete your Todo-List?");
+    if(confirmDelete){
+      localStorage.removeItem("todos");
+      setAllTodos([]);
+      localStorage.removeItem("id");
+      setID(0);
+    }
+  }
 
 
 
-
-  return(
+  return (
     <div className="App">
       <div className="App-header">
 
         <Navigation />
 
+        <br></br>
+        <center>
+
+          <h1 style={{ fontSize: "24pt" }}>Todo List</h1>
+
           <br></br>
-          <center>
 
-          <h1 style={{fontSize:"24pt"}}>Todo List</h1>
-
-          <br></br>
-          
-          <Form addTodo = {addTodo} />
+          <Form addTodo={addTodo} />
 
 
-          
-          {todos.length>0 &&
-              <Todolist todos = {todos} lineText = {lineText} removeTodo = {removeTodo} />
-            }
-          
-          </center>
-          <br></br><br></br>
+
+          {todos.length > 0 &&
+            <Todolist todos={todos} lineText={lineText} removeTodo={removeTodo} deleteList={deleteList} />
+          }
+
+        </center>
+        <br></br><br></br>
       </div>
-      
-     
-    
-        
+
+
+
+
 
     </div>
   )
--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -2,69 +2,76 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
 
-export default function Todolist(props){
 
-    const openTodos = props.todos.filter(todo => todo.isOpen);
-    const doneTodos = props.todos.filter(todo => !todo.isOpen);
+export default function Todolist(props) {
+
+  const openTodos = props.todos.filter(todo => todo.isOpen);
+  const doneTodos = props.todos.filter(todo => !todo.isOpen);
 
 
-    return(
-        <div className="todolist-background">
-        
-        <ul className="todo-list">
+  return (
+    <div className="todolist-background">
+
+      <ul className="todo-list">
         <h3>Open Todos: {openTodos.length}/{props.todos.length}</h3>
-        
-        
 
-            <div className="open-todos-div">
-            
-              {props.todos.length>0 && openTodos.length === 0 && 
-                <div className="completion-div">
-                  <br></br>
-                  Well done! 
+
+
+        <div className="open-todos-div">
+          {props.todos.length > 0 && openTodos.length === 0 &&
+            <div className="completion-div">
+              <br></br>
+                  Well done!
                   <br></br>
                   All tasks completed!
                   <br></br>
-                  <FontAwesomeIcon icon={faCheck} color="green" />
-                </div>
-                }
+              <FontAwesomeIcon icon={faCheck} color="green" />
 
-              {openTodos.map((openTodo, index) => {
-                  
-                  return(
-                    <li key={index} id={openTodo.id} className="todo-listitem">
-                      <button  className={openTodo.isOpen ? "open" : "done"}  id={openTodo.id} onClick={props.lineText}>{openTodo.text}</button>
-                      <button  className="delete-BTN" id={openTodo.id} onClick={props.removeTodo}><FontAwesomeIcon icon={faTrash} color="grey" /></button>
-                    </li>
-                  )}  
-                  )}
             </div>
+          }
 
-              <br></br>
-              <br></br>
-              <hr></hr>
+          {openTodos.map((openTodo, index) => {
 
-              <h3>Done</h3>
-     
-              {doneTodos.map((filteredTodo, index) => {
-                  
-                  return(
-                  
-                    <li key={index} id={filteredTodo.id} className="todo-listitem">
-                      <button  className={filteredTodo.isOpen ? "open" : "done"} id={filteredTodo.id} onClick={props.lineText}>{filteredTodo.text}</button>
-                      <button  className="delete-BTN" id={filteredTodo.id} onClick={props.removeTodo}><FontAwesomeIcon icon={faTrash} color="grey" /></button>
-                    </li>
-                  
-                  )}  
-                  )}
-            
-              
-         
-              
-        </ul>
-
+            return (
+              <li key={index} id={openTodo.id} className="todo-listitem">
+                <button className={openTodo.isOpen ? "open" : "done"} id={openTodo.id} onClick={props.lineText}>{openTodo.text}</button>
+                <button className="delete-BTN" id={openTodo.id} onClick={props.removeTodo}><FontAwesomeIcon icon={faTrash} color="grey" /></button>
+              </li>
+            )
+          }
+          )}
         </div>
-    )
+
+        <br></br>
+        <br></br>
+        <hr></hr>
+
+        <h3>Done</h3>
+
+        {doneTodos.map((filteredTodo, index) => {
+
+          return (
+
+            <li key={index} id={filteredTodo.id} className="todo-listitem">
+              <button className={filteredTodo.isOpen ? "open" : "done"} id={filteredTodo.id} onClick={props.lineText}>{filteredTodo.text}</button>
+              <button className="delete-BTN" id={filteredTodo.id} onClick={props.removeTodo}><FontAwesomeIcon icon={faTrash} color="grey" /></button>
+            </li>
+
+          )
+        }
+        )}
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <button className="delete-list-BTN" onClick={props.deleteList}>Delete list</button>
+        <br></br>
+        <br></br>
+
+      </ul>
+
+    </div>
+  )
 }
 
 
