57eeb4f079dc71b042493df3adbf7c6f1621966c
Christian Eckardt <eckardt.christian@web.de>
2021-04-01 19:58:06 +0200

    nav fixes

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,68 +1,64 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 
-export default function Navigation(){
+export default function Navigation() {
 
-    const [menuShown, toggleShowMenu] = useState("no-show");
-    const [burgerBackground, toggleBurger] = useState("hamburger");
+  const [menuShown, toggleShowMenu] = useState("no-show");
+  const [burgerBackground, toggleBurger] = useState("hamburger");
 
-    function showMenu(event){
-        if (menuShown === "no-show") {
-        toggleShowMenu("show-menu");
-        toggleBurger("greenburger");
-        }
-        
-        if(menuShown === "show-menu"){
-        toggleShowMenu("no-show");
-        toggleBurger("hamburger");
-        }
+  function showMenu(event) {
+    if (menuShown === "no-show") {
+      toggleShowMenu("show-menu");
+      toggleBurger("greenburger");
     }
 
-    return(
-        <nav>
- 
-        <button className={burgerBackground} onClick={showMenu}>
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </button>
-        <div id="burger-menu" show={menuShown} className={menuShown}>
-          <ul>
-            <li>
-              <a href="https://eckeecke.github.io/index.html">Home</a>
-            </li>
+    if (menuShown === "show-menu") {
+      toggleShowMenu("no-show");
+      toggleBurger("hamburger");
+    }
+  }
 
-            <li>
-              <a href="https://nifty-hopper-c1da01.netlify.app/" target="_blank">React Travel Blog</a>
-            </li>
+  return (
+    <nav>
 
-            <li>
-              <a href="https://eckeecke.github.io/shooter.html">JS Space Shooter</a>
-            </li>
- 
-            <li>
-              <a href="https://eckeecke.github.io/pong.html">JS Pong</a>
-            </li>
+      <button className={burgerBackground} onClick={showMenu}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </button>
+      <div id="burger-menu" show={menuShown} className={menuShown}>
+        <ul>
+          <li>
+            <a href="https://eckeecke.github.io/index.html">Home</a>
+          </li>
 
-            <li>
-              <a href="https://adoring-galileo-71fde2.netlify.app/">React Todo App</a>
-            </li>
+          <li>
+            <a href="https://nifty-hopper-c1da01.netlify.app/" target="_blank">React Travel Blog</a>
+          </li>
 
-        <li>
-          <a href="newgame.html">Drawing App</a>
-        </li>
- 
-        <li>
-          <a href="templelooter.html">Temple Looter</a>
-        </li>
+          <li>
+            <a href="https://eckeecke.github.io/shooter.html">JS Space Shooter</a>
+          </li>
 
-        <li>
-          <a href="https://nervous-elion-4e903f.netlify.app/" target="_blank">Vue Meal Planner</a>
-        </li>
-          </ul>
-        </div>
+          <li>
+            <a href="https://eckeecke.github.io/pong.html">JS Pong</a>
+          </li>
+          
+          <li>
+            <a href="newgame.html">Drawing App</a>
+          </li>
 
-      </nav>
-    )
+          <li>
+            <a href="templelooter.html">Temple Looter</a>
+          </li>
+
+          <li>
+            <a href="https://nervous-elion-4e903f.netlify.app/" target="_blank">Vue Meal Planner</a>
+          </li>
+        </ul>
+      </div>
+
+    </nav>
+  )
 }
